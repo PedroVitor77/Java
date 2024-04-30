4e5506af964cb7f98cf1c89808a2ad09fc4d92d7
PedroVitor77 <pedrovitordev.contato@gmail.com>
2024-04-29 21:28:18 -0300

    novos feature

--- JavaBasico/src/BoletimEstudantil.java
+++ JavaBasico/src/BoletimEstudantil.java
@@ -1,6 +1,6 @@
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 7;
+        int mediaFinal = 10;
         if (mediaFinal < 6)
             System.out.println("Reprovado");
         else if (mediaFinal == 6)
