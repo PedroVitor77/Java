bb94520d189d6756485de3c9c8af79a90f305e91
PedroVitor77 <pedrovitordev.contato@gmail.com>
2024-05-06 21:01:39 -0300

    novas aulas

---
+++ JavaBasico/src/edu/pedro/Condicionais/Estrutura condicionais/src/condicionaisSimples.java
@@ -0,0 +1,5 @@
+public class  condicionaisSimples {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ JavaBasico/src/edu/pedro/Condicionais/Estrutura condicionais/src/operadora.java
@@ -0,0 +1,23 @@
+
+public class operadora {
+	public static void main(String[] args) {
+		String plano = "M";
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5gb YouTube");
+				break;
+			}
+			case "M": {
+				System.out.println("whatsApp e instagram gratis");
+				break;
+			}
+			case "B": {
+				System.out.println("100min de Ligacao gratis");
+				break;
+			}
+
+		}
+
+	}
+}
